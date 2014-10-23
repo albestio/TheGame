cde794e64dbbd6c5361518e9a2d0306bf2a8ae05
jgolla <jason@golla.me>
2014-10-22 23:59:22 -0400

    code cleanup

--- src/script/game.js
+++ src/script/game.js
@@ -1,31 +1,32 @@
-window.addEventListener("load", function() {
-  var Q = window.Q = Quintus({development: true})
-    .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
-    //.include("ActionPlatformerPlayer, ActionPlatformerEnemy")
-    .setup({
-      width: 2000,   //to fit devices with a screne resolution of 1280 x 720
-      height: 2000,
-      scaleToFit: true
-    }).controls().touch();
-    
+window.addEventListener("load", function () {
+    var Q = window.Q = Quintus({development: true})
+        .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
+        .setup({
+            width: 2000,
+            height: 2000,
+            scaleToFit: true
+        })
+        .controls()
+        .touch();
     Q.setImageSmoothing(false);
     
     Q.Sprite.extend("Player", {
-        init: function(p) {
-            this._super(p, { x: 10, y: 10, asset: 'player.PNG', jumpSpeed: -400 });
-            this.add('2d, platformerControls');
+        init: function (p) {
+            this._super(p, { x: 0, y: 0, asset: "player.PNG", jumpSpeed: -400 });
+            this.add("2d, platformerControls");
         },
-        step: function(dt) {
+        step: function (dt) {
             
-            var getTileLocation = function(x,y) {
-                var x1 = Math.floor(x / 80);
-                var y1 = Math.floor(y / 80);
+            var getTileLocation = function (x, y) {
+                var x1 = Math.floor(x / 80),
+                    y1 = Math.floor(y / 80);
+                
                 return {x: x1, y: y1};
             };
             
-            if(Q.inputs['down']) {
+            if (Q.inputs.down) {
                 var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h, Q.SPRITE_DEFAULT);
-                if(ground && ground.p) {
+                if (ground && ground.p) {
                     var point = getTileLocation(this.p.x, this.p.y);
                     ground.p.tiles[point.y + 1][point.x] = null;
                 }
@@ -33,14 +34,14 @@ window.addEventListener("load", function() {
         }
     });
 
-    Q.scene("level",function(stage) {
-        Q.stageTMX("level1.tmx",stage); 
+    Q.scene("level", function (stage) {
+        Q.stageTMX("level1.tmx", stage);
         stage.insert(new Q.Player());
-        stage.add("viewport");      
+        stage.add("viewport");
     });
 
     //load assets
-    Q.loadTMX("level1.tmx, player.PNG", function() {       
-      Q.stageScene("level");
+    Q.loadTMX("level1.tmx, player.PNG", function () {
+        Q.stageScene("level");
     });
 });
